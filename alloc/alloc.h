5c95d6101bbc67eb5b35e786b62bc686691d06f3
wujiahao <wujiahao.mail@gmail.com>
2021-05-15 23:09:19 +0800

    完成测试

--- alloc/alloc.h
+++ alloc/alloc.h
@@ -41,6 +41,48 @@ public:
     }
 };
 
+template <int inst>
+void *malloc_alloc_template<inst>::oom_malloc(size_t n)
+{
+    void (*my_malloc_handler)();
+    void *result;
+    while (1)
+    {
+        my_malloc_handler = malloc_alloc_oom_handler;
+        if (0 == my_malloc_handler)
+        {
+            throw std::bad_alloc();
+        }
+        (*my_malloc_handler)();
+        result = malloc(n);
+        if (result)
+            return result;
+    }
+}
+
+template <int inst>
+void *malloc_alloc_template<inst>::oom_realloc(void *p, size_t n)
+{
+    void (*my_malloc_handler)();
+    void *result;
+    while (1)
+    {
+        my_malloc_handler = malloc_alloc_oom_handler;
+        if (0 == my_malloc_handler)
+        {
+            throw std::bad_alloc();
+        }
+        // 执行一次my_malloc_handler
+        (*my_malloc_handler)();
+        result = realloc(p, n);
+        if (result)
+            return result;
+    }
+}
+
+template <int inst>
+void (*malloc_alloc_template<inst>::malloc_alloc_oom_handler)() = nullptr;
+
 typedef malloc_alloc_template<0> malloc_alloc;
 
 template <typename T>
@@ -126,48 +168,6 @@ private:
 };
 
 template <int inst>
-void *malloc_alloc_template<inst>::oom_malloc(size_t n)
-{
-    void (*my_malloc_handler)();
-    void *result;
-    while (1)
-    {
-        my_malloc_handler = malloc_alloc_oom_handler;
-        if (0 == my_malloc_handler)
-        {
-            throw std::bad_alloc();
-        }
-        (*my_malloc_handler)();
-        result = malloc(n);
-        if (result)
-            return result;
-    }
-}
-
-template <int inst>
-void *malloc_alloc_template<inst>::oom_realloc(void *p, size_t n)
-{
-    void (*my_malloc_handler)();
-    void *result;
-    while (1)
-    {
-        my_malloc_handler = malloc_alloc_oom_handler;
-        if (0 == my_malloc_handler)
-        {
-            throw std::bad_alloc();
-        }
-        // 执行一次my_malloc_handler
-        (*my_malloc_handler)();
-        result = realloc(p, n);
-        if (result)
-            return result;
-    }
-}
-
-template <int inst>
-void (*malloc_alloc_template<inst>::malloc_alloc_oom_handler)() = nullptr;
-
-template <int inst>
 void *malloc_alloc_template<inst>::allocate(size_t n)
 {
     void *result = malloc(n);
@@ -201,13 +201,13 @@ template <typename T>
 size_t myAllocator<T>::roundUp(size_t bytes)
 {
     // +7在把二进制表示的后三位置位0
-    return (((bytes) + (size_t)ALIGN - 1) & ~(size_t)ALIGN - 1);
+    return (((bytes) + (size_t)ALIGN - 1) & ~((size_t)ALIGN - 1));
 }
 
 template <typename T>
 size_t myAllocator<T>::freeListIndex(size_t bytes)
 {
-    return (((bytes) + (size_t)ALIGN - 1) / (size_t)ALIGN - 1);
+    return (((bytes) + (size_t)ALIGN - 1) / ((size_t)ALIGN - 1));
 }
 
 template <typename T>
@@ -233,14 +233,21 @@ void *myAllocator<T>::refill(size_t n)
     result = (Object *)chunk;
     *myFreeList = nextObject = (Object *)(chunk + n);
     // 还剩下nObjects-1个块，需要建立nObjects-2个链接
-    for (i = 2; i < nObjects; ++i)
+    for (i = 1;; ++i)
     {
         currentObject = nextObject;
         nextObject = (Object *)((char *)nextObject + n);
-        currentObject->freeListLink = nextObject;
+        if (i == nObjects - 1)
+        {
+            currentObject->freeListLink = 0;
+            break;
+        }
+        else
+        {
+            currentObject->freeListLink = nextObject;
+        }
     }
     // 末尾置为空
-    nextObject->freeListLink = 0;
     return result;
 }
 
@@ -273,21 +280,23 @@ char *myAllocator<T>::chunkAlloc(size_t size, int &nObjects)
     {
         // 剩下的内存连一个区块的大熊啊都无法提供
         // 这是要通过malloc获取的字节数
-        size_t bytesToGet = 2 * totalBytes + roundUp(heapSize >> 4);
+        size_t bytesToGet =
+            2 * totalBytes + roundUp(heapSize >> 4);
         if (bytesLeft > 0)
         {
             // 内存池还有一些零头，先配给适当的freelist
             // 首先找到适合的freelist
-            Object *volatile *myFreeList = freeList + freeListIndex(bytesLeft);
+            Object *volatile *myFreeList =
+                freeList + freeListIndex(bytesLeft);
             // 将剩下的chunk插入到freelist中
             ((Object *)startFree)->freeListLink = *myFreeList;
             *myFreeList = (Object *)startFree;
         }
         startFree = (char *)malloc(bytesToGet);
-        if (0 == startFree)
+        if (nullptr == startFree)
         {
             // malloc分配空间失败
-            int i;
+            size_t i;
             Object *volatile *myFreeList;
             Object *p;
             for (i = size; i <= (size_t)MAX_CHUNK;
@@ -330,20 +339,18 @@ T *myAllocator<T>::allocate(size_t n)
     {
         // 找到对应的list
         Object *volatile *myFreeList = freeList + freeListIndex(n);
+        // 建立一个临界区
+        std::lock_guard<std::mutex> guard(mtx);
+        Object *result = *myFreeList;
+        if (result == 0)
         {
-            // 建立一个临界区
-            std::lock_guard<std::mutex> guard(mtx);
-            Object *result = *myFreeList;
-            if (result == 0)
-            {
-                // 链表为空
-                res = refill(roundUp(n));
-            }
-            else
-            {
-                *myFreeList = result->freeListLink;
-                res = result;
-            }
+            // 链表为空
+            res = refill(roundUp(n));
+        }
+        else
+        {
+            *myFreeList = result->freeListLink;
+            res = result;
         }
     }
     return (T *)res;
@@ -354,7 +361,7 @@ void myAllocator<T>::deallocate(void *p, size_t n)
 {
     if (n > (size_t)MAX_CHUNK)
     {
-        // 大于最大chunk，直接带哦用一级空间配置器
+        // 大于最大chunk，直接调用一级空间配置器
         malloc_alloc::dellocate(p);
     }
     else
@@ -375,7 +382,8 @@ void *myAllocator<T>::reallocate(void *p, size_t oldSize, size_t newSize)
 {
     void *result;
     size_t copySize;
-    if (oldSize > (size_t)MAX_CHUNK && (size_t)MAX_CHUNK)
+
+    if (oldSize > (size_t)MAX_CHUNK && newSize > (size_t)MAX_CHUNK)
     {
         // 超出最大块的字节数直接调用realloc
         return (realloc(p, newSize));
@@ -387,11 +395,11 @@ void *myAllocator<T>::reallocate(void *p, size_t oldSize, size_t newSize)
     }
     // 重新分配内存
     result = allocate(newSize);
-    copySize = newSize;
+    copySize = newSize > oldSize ? oldSize : newSize;
     // 讲之前的内存赋值过来
     memcpy(result, p, copySize);
     deallocate(p, oldSize);
-    return result;
+    return (result);
 }
 
 template <typename T>
--- alloc/test.cpp
+++ alloc/test.cpp
@@ -1,20 +1,48 @@
 #include "alloc.h"
 #include <vector>
+#include <ctime>
+#include <chrono>
 using namespace std;
+using namespace chrono;
+
+struct node
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int f;
+    int g;
+    int nums[100];
+};
+
+typedef node type;
+#define times 10000
+
+vector<type, myAllocator<type>> vec1;
+vector<type> vec2;
 
 int main()
 {
-    vector<int, myAllocator<int>> vec;
-    cout << "construct suscess" << endl;
-    for (int i = 0; i < 5; ++i)
+    system_clock::time_point start1, end1, start2, end2;
+    start1 = system_clock::now();
+    type tmp;
+    for (int i = 0; i < times; ++i)
     {
-        vec.push_back(1);
+        vec1.push_back(tmp);
     }
-    cout << "push_back suscess" << endl;
-    for (int val : vec)
+    end1 = system_clock::now();
+    system_clock::duration duration1 = end1.time_since_epoch() - start1.time_since_epoch();
+    start2 = system_clock::now();
+    for (int i = 0; i < times; ++i)
     {
-        cout << val << ' ';
+        vec2.push_back(tmp);
     }
-    cout << endl;
+    end2 = system_clock::now();
+    system_clock::duration duration2 = end2.time_since_epoch() - start2.time_since_epoch();
+    time_t microseconds_since_epoch = duration_cast<microseconds>(duration1).count();
+    cout << microseconds_since_epoch << endl;
+    microseconds_since_epoch = duration_cast<microseconds>(duration2).count();
+    cout << microseconds_since_epoch << endl;
     return 0;
 }
